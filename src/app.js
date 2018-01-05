3ae66a9a7b55d3a7b7fd5117ebaaa8e9db5cd4b8
Junior Klegseth <jklegseth@gmail.com>
2018-01-04 20:34:41 -0800

    remove dummy data

--- src/app.js
+++ src/app.js
@@ -13,17 +13,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 7800, createdAt: 1513641600}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 3700, createdAt: 1513661600}));
-store.dispatch(addExpense({ description: 'Rent', amount: 19500, createdAt: 1503661600}));
-
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
